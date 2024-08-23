d028fbf73896a228aad40c478361edc49b266fd0
Erik <ewpeters@asu.edu>
2024-08-22 21:34:44 -0700

    remove cache testing

--- app/javascript/cache_processor.js
+++
@@ -1,8 +0,0 @@
-if ('caches' in window) {
-  // Cache API is supported
-  console.log("Well I'll be damned! :)")
- //You can add your code here
-} else {
-  console.log("Fuck me! >:(")
-  // Cache API is not supported
-}
