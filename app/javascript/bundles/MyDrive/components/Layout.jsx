6a28a876fb6c4062738f4b6d781ede8c64e52248
Erik <ewpeters@asu.edu>
2024-10-26 22:01:47 -0700

    Wrap routes component in layout

---
+++ app/javascript/bundles/MyDrive/components/Layout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink as Link } from 'react-router-dom';
+
+const Layout = ({ children }) => {
+
+  return (
+    <section className="max-w-none">
+      <header>
+        <ul>
+          <li>
+            <Link end to="/" className={({ isActive }) => (isActive ? ' activated' : '')}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/users/sign_in" className={({ isActive }) => (isActive ? ' activated' : '')}>
+              Sign In
+            </Link>
+          </li>
+        </ul>
+      </header>
+      {children}
+    </section>
+  );
+
+}
+
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.object]).isRequired;
+}
+
+export default Layout;
--- app/javascript/bundles/MyDrive/routes/routes.jsx
+++ app/javascript/bundles/MyDrive/routes/routes.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../components/LandingPage.jsx';
 import Login from '../components/Login.jsx';
+import Layout from '../layout/Layout.jsx'
 
 export default (
-  <Routes>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/users/sign_in" element={<Login />} />
-  </Routes>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/users/sign_in" element={<Login />} />
+    </Routes>
+  </Layout>
 );
