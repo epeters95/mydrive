2d2334dfd4049afbc630e9297e5b238c392ab353
Erik <ewpeters@asu.edu>
2024-10-28 23:31:28 -0700

    redirect rails home to react home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
-    @latest_album = Album.last
-    render :index
+    # @latest_album = Album.last
+    # render :index
+    render "devise/sessions/new"
   end
 
 end
