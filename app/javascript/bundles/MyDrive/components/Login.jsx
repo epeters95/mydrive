3b6313759172046a8ee3973910b7749e91395589
Erik <ewpeters@asu.edu>
2024-10-30 19:08:58 -0700

    fix credentials format in params, redirect to albums#index

--- app/javascript/bundles/MyDrive/components/Login.jsx
+++ app/javascript/bundles/MyDrive/components/Login.jsx
@@ -2,14 +2,12 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import * as style from './Credentials.module.css';
 import ReactOnRails from 'react-on-rails';
-import { useNavigate } from 'react-router-dom';
+import { redirect } from 'react-router-dom';
 
 const Login = (props) => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
-  let navigate = useNavigate();
 
   const submitLogin = () => {
     fetch('http://localhost:3000/users/sign_in', {
@@ -19,17 +17,19 @@ const Login = (props) => {
         'X-CSRF-Token': ReactOnRails.authenticityToken()
       },
       body: JSON.stringify({
-       "email":    email,
-       "password": password
+        "user": {
+          "email":    email,
+          "password": password
+        }
       }),
     })
     .then((resp) => {
 
       if (resp.status === 200) {
-        // props.setEmail(email)
-        // navigate('/') 
-        console.log(resp)
+
         window.alert("Login success!")
+        redirect("/albums");
+
       } else if (resp.status === 422) {
         window.alert('Unauthorized request')
       } else {
@@ -48,6 +48,7 @@ const Login = (props) => {
 
       <br/>
       <input type='password' value={password}
+            name="password"
             placeholder="password"
             onChange={(e) => setPassword(e.target.value)}
             className={'credential-field'} />
--- app/javascript/bundles/MyDrive/routes/routes.jsx
+++ app/javascript/bundles/MyDrive/routes/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import LandingPage from '../components/LandingPage.jsx';
 import Login from '../components/Login.jsx';
 import Layout from '../components/Layout.jsx'
@@ -9,6 +9,8 @@ export default (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/users/sign_in" element={<Login />} />
+      <Route path="/albums" element={<Navigate to="/" />}
+        />
     </Routes>
   </Layout>
 );
