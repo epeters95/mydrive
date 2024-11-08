45d4e0a90c75145e0da045d13d900efda436eddf
Erik <ewpeters@asu.edu>
2024-11-07 22:38:14 -0700

    switch from BrowserRouter to createBrowserRouter

--- app/javascript/bundles/MyDrive/components/RouterApp.jsx
+++ app/javascript/bundles/MyDrive/components/RouterApp.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import routes from '../routes/routes.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LandingPage from '../components/LandingPage.jsx';
 
 const RouterApp = (props) => {
 
+  const albumsLoader = () => {
+    return fetch('http://localhost:3000/albums', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  const editAlbumLoader = ({ params }) => {
+    return fetch('http://localhost:3000/albums/' + params.albumId + '/edit', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+      children: [
+        {
+          path: "team",
+          element: <Team />,
+          loader: teamLoader,
+        },
+      ],
+    },
+    {
+      path: "/users/sign_in",
+      element: <Login />
+    },
+    {
+      path: "/albums",
+      element: <AlbumsList />,
+      loader: albumsLoader,
+      children: [
+        {
+          path: "/:albumId/edit",
+          element: <EditAlbum />,
+          loader: editAlbumLoader,
+        },
+      ],
+    }
+  ]);
+
   return (
-    <BrowserRouter>
-      {routes}
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
+
 
 export default RouterApp;
