ee120fcd9f52f07568e3cdc409463947d816efae
Erik <ewpeters@asu.edu>
2025-07-04 23:38:00 -0700

    add placeholder for comment deletion endpoint

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+
+  def destroy
+
+    # comment = Comment.find(params[:id])
+    # if comment
+    #   comment.destroy
+    # else
+    #   render json: :error
+    # end
+    # render json: :success
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 
 
   resources :albums do
-    resources :photos
+    resources :photos do
+      resources :comments
+    end
   end
     
 
