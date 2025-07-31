31184af56fa1d312d8ae8d5a0984cf17709f314f
Erik <ewpeters@asu.edu>
2025-07-30 19:21:02 -0700

    add in placeholder file to keep server rendering boilerplates

---
+++ app/javascript/packs/server-bundle.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+// import Photo from '../bundles/MyDrive/components/PhotoServer';
+
+// No specialized server rendering yet
+// ReactOnRails.register({
+//   PhotoServer,
+// });
