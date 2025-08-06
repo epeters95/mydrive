bb6c1bf76f63b0b01efdae1a13166e4dc896138f
Erik <ewpeters@asu.edu>
2025-08-05 23:57:53 -0700

    fix duplicate plugins issue

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,23 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+// const { generateWebpackConfig } = require('shakapacker')
 
-const webpackConfig = generateWebpackConfig()
+// const webpackConfig = generateWebpackConfig()
 
-module.exports = webpackConfig
+// module.exports = webpackConfig
+
+
+const { env } = require('shakapacker')
+const { existsSync } = require('fs')
+const { resolve } = require('path')
+
+const envSpecificConfig = () => {
+  const path = resolve(__dirname, `${env.nodeEnv}.js`)
+  if (existsSync(path)) {
+    console.log(`Loading ENV specific webpack configuration file ${path}`)
+    return require(path)
+  } else {
+    throw new Error(`Could not find file to load ${path}, based on NODE_ENV`)
+  }
+}
+
+module.exports = envSpecificConfig()
