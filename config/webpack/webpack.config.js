5c3373ada838025aa61f45117b91125bb17e2410
Erik <ewpeters@asu.edu>
2025-08-10 23:24:48 -0700

    finish switching from babel to swc and webpack options

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,23 +1,30 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-// const { generateWebpackConfig } = require('shakapacker')
+const { generateWebpackConfig } = require('shakapacker')
 
-// const webpackConfig = generateWebpackConfig()
-
-// module.exports = webpackConfig
-
-
-const { env } = require('shakapacker')
-const { existsSync } = require('fs')
-const { resolve } = require('path')
-
-const envSpecificConfig = () => {
-  const path = resolve(__dirname, `${env.nodeEnv}.js`)
-  if (existsSync(path)) {
-    console.log(`Loading ENV specific webpack configuration file ${path}`)
-    return require(path)
-  } else {
-    throw new Error(`Could not find file to load ${path}, based on NODE_ENV`)
+const options = {
+  resolve: {
+      extensions: ['.css', '.js', '.jsx']
   }
 }
 
-module.exports = envSpecificConfig()
+
+const webpackConfig = generateWebpackConfig(options)
+
+module.exports = webpackConfig
+
+
+// const { env } = require('shakapacker')
+// const { existsSync } = require('fs')
+// const { resolve } = require('path')
+
+// const envSpecificConfig = () => {
+//   const path = resolve(__dirname, `${env.nodeEnv}.js`)
+//   if (existsSync(path)) {
+//     console.log(`Loading ENV specific webpack configuration file ${path}`)
+//     return require(path)
+//   } else {
+//     throw new Error(`Could not find file to load ${path}, based on NODE_ENV`)
+//   }
+// }
+
+// module.exports = envSpecificConfig()
