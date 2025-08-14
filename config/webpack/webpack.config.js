16a6290c7533f12e66e2e8935042bf6babcc83ce
Erik <ewpeters@asu.edu>
2025-08-13 23:46:00 -0700

    use demo versions of config files

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,30 +1,15 @@
-// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+const { env, generateWebpackConfig } = require('shakapacker');
 
-const options = {
-  resolve: {
-      extensions: ['.css', '.js', '.jsx']
+const { existsSync } = require('fs');
+const { resolve } = require('path');
+
+const envSpecificConfig = () => {
+  const path = resolve(__dirname, `${env.nodeEnv}.js`);
+  if (existsSync(path)) {
+    console.log(`Loading ENV specific webpack configuration file ${path}`);
+    return require(path);
   }
-}
+  return generateWebpackConfig();
+};
 
-
-const webpackConfig = generateWebpackConfig(options)
-
-module.exports = webpackConfig
-
-
-// const { env } = require('shakapacker')
-// const { existsSync } = require('fs')
-// const { resolve } = require('path')
-
-// const envSpecificConfig = () => {
-//   const path = resolve(__dirname, `${env.nodeEnv}.js`)
-//   if (existsSync(path)) {
-//     console.log(`Loading ENV specific webpack configuration file ${path}`)
-//     return require(path)
-//   } else {
-//     throw new Error(`Could not find file to load ${path}, based on NODE_ENV`)
-//   }
-// }
-
-// module.exports = envSpecificConfig()
+module.exports = envSpecificConfig();
