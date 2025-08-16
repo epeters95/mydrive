b8e17441b14747e76a2fe8f0c37725396776479b
Erik <ewpeters@asu.edu>
2025-08-15 22:43:56 -0700

    final working homepage, restore rest of app

--- app/javascript/packs/mydrive-bundle.js
+++ app/javascript/packs/mydrive-bundle.js
@@ -1,34 +1,34 @@
 import ReactOnRails from 'react-on-rails';
 
-// import Album from '../bundles/MyDrive/components/album/Album';
-// import AlbumsList from '../bundles/MyDrive/components/album/AlbumsList';
-// import EditAlbum from '../bundles/MyDrive/components/album/EditAlbum';
-// import NewAlbum from '../bundles/MyDrive/components/album/NewAlbum';
-// import Photo from '../bundles/MyDrive/components/photo/Photo';
-// import Comment from '../bundles/MyDrive/components/photo/Comment';
-// import Login from '../bundles/MyDrive/components/auth/Login';
-// import Signup from '../bundles/MyDrive/components/auth/Signup';
+import Album from '../bundles/MyDrive/components/album/Album';
+import AlbumsList from '../bundles/MyDrive/components/album/AlbumsList';
+import EditAlbum from '../bundles/MyDrive/components/album/EditAlbum';
+import NewAlbum from '../bundles/MyDrive/components/album/NewAlbum';
+import Photo from '../bundles/MyDrive/components/photo/Photo';
+import Comment from '../bundles/MyDrive/components/photo/Comment';
+import Login from '../bundles/MyDrive/components/auth/Login';
+import Signup from '../bundles/MyDrive/components/auth/Signup';
 import LandingPage from '../bundles/MyDrive/components/LandingPage';
 import RouterApp from '../bundles/MyDrive/components/RouterApp';
 import Layout from '../bundles/MyDrive/components/Layout';
-// import AllUsers from '../bundles/MyDrive/components/auth/AllUsers';
-// import CommentsList from '../bundles/MyDrive/components/photo/CommentsList';
+import AllUsers from '../bundles/MyDrive/components/auth/AllUsers';
+import CommentsList from '../bundles/MyDrive/components/photo/CommentsList';
 
 import './bootstrap.bundle.min.js';
 import './bootstrap.min.css';
 
 ReactOnRails.register({
-  // Album,
-  // Photo,
-  // Comment,
-  // Login,
-  // Signup,
+  Album,
+  Photo,
+  Comment,
+  Login,
+  Signup,
   LandingPage,
   RouterApp,
-  Layout
-  // AlbumsList,
-  // EditAlbum,
-  // NewAlbum,
-  // AllUsers,
-  // CommentsList
+  Layout,
+  AlbumsList,
+  EditAlbum,
+  NewAlbum,
+  AllUsers,
+  CommentsList
 });
