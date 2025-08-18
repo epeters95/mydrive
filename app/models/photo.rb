2d492ea2debad7cd38f37c748a4b484ae11bb083
Erik <ewpeters@asu.edu>
2025-08-17 22:50:15 -0700

    convenience method for photo index ui

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,14 @@ class Photo < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  def self.latest_commented
+    Photo.recent_activity.first
+  end
+
+  def self.recent_activity
+    Photo.find_by_sql("SELECT * FROM photos INNER JOIN comments ON comments.photo_id = photos.id ORDER BY comments.created_at DESC")
+  end
+
   def to_object
     {
       name:        self.name,
