f74e1817c36f198da503ccbecfdc3da2de701f19
Erik <ewpeters@asu.edu>
2025-09-18 22:51:29 -0700

    add shares to text method

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,4 +2,12 @@ class Share < ApplicationRecord
   has_many :photos, through: :photo_share
   has_many :users, through: :photo_share, class_name: "user"
   belongs_to :sharer, class_name: "user"
+
+  def to_text
+    lines = []
+    self.photo_shares.each do |ps|
+      lines << "#{ps.photo.name}, #{ps.photo.created_at.strftime("%FT%T")} with #{ps.user.name} at #{self.created_at.strftime("%FT%T")}"
+    end
+    lines.join("\n")
+  end
 end
