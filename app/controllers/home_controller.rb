d8108a7e4b1f3ec18cead41842afbdb7f3fec4cd
Erik <ewpeters@asu.edu>
2025-09-22 23:45:13 -0700

    incorrect concat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @shared_with = []
-
-    @shared_with << Share.joins(:user).find(user_id: current_user.id).to_a.map{|sw| sw.to_object }
+    @shared_with = Share.joins(:user).find(user_id: current_user.id).to_a.map{|sw| sw.to_object }
     # TODO: verify each model reference used by view is in nested json e.g. photo.user.name
     render :index
   end
