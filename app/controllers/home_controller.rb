7453386856bb9a7510f8439d7f6a897cf00e096e
Erik <ewpeters@asu.edu>
2025-09-22 23:43:00 -0700

    restore test landing page to visualize shares

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
+  before_action :authenticate_user
+
   def index
-    render "devise/sessions/new"
+    @shared_with = []
+
+    @shared_with << Share.joins(:user).find(user_id: current_user.id).to_a.map{|sw| sw.to_object }
+    # TODO: verify each model reference used by view is in nested json e.g. photo.user.name
+    render :index
   end
 
 end
