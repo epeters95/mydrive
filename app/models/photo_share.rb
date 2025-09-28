f3356d9e2113bcab49c598d892847644f5aeb2f6
Erik <ewpeters@asu.edu>
2025-09-27 23:08:22 -0700

    refactor to model

--- app/models/photo_share.rb
+++ app/models/photo_share.rb
@@ -2,4 +2,13 @@ class PhotoShare < ApplicationRecord
   belongs_to :share
   belongs_to :user, dependent: :destroy
   belongs_to :photo
+
+  def to_object
+    {
+      id: self.id,
+      photo: self.photo.to_object,
+      user: self.user.to_object,
+      created_at: self.created_at
+    }
+  end
 end
