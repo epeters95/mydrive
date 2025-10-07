73af5096215680bb1f566c35b70045a16c98081c
Erik <ewpeters@asu.edu>
2025-10-06 20:17:12 -0700

    share destroy method

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,5 +1,21 @@
 class SharesController < ApplicationController
+  before_action :authenticate_user!
 
+  def destroy
+
+    share = Share.find(params[:id])
+    if share
+      if current_user.id == share.sharer.id
+        share.destroy
+        render json: { message: "successfully deleted" }, status: :ok
+      else
+        render json: { error: "Only the sharer can delete this share" }, status: :unauthorized
+      end
+    else
+      render json: { error: "couldn't find share" }, status: :not_found
+    end
+  end
+  
   def all_shares_text
     render json: { shares: Share.all.order(created_at: "desc").map{|sh| sh.to_text } }, status: :ok
   end
