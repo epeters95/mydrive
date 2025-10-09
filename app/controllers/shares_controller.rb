f1fdf61cf86e6f2f71597fc5d9e385babae4a241
Erik <ewpeters@asu.edu>
2025-10-08 22:29:00 -0700

    share update

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -15,6 +15,20 @@ class SharesController < ApplicationController
       render json: { error: "couldn't find share" }, status: :not_found
     end
   end
+
+  def update
+
+    share = Share.find(share_params[:id])
+    if share && current_user.id == share.sharer.id
+      if share.update(sharer_id: share_params[:sharer_id])
+        render json: { message: "successfully updated" }, status: :ok
+      else
+        render json: { error: "Unable to update share" }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "couldn't find share" }, status: :not_found
+    end
+  end
   
   def all_shares_text
     render json: { shares: Share.all.order(created_at: "desc").map{|sh| sh.to_text } }, status: :ok
@@ -22,7 +36,7 @@ class SharesController < ApplicationController
 
   private
   def share_params
-    params.require(:share).permit(:id)
+    params.require(:share).permit(:id, :sharer_id)
   end
   
 end
