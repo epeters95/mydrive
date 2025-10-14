ccae128dd656f30dee7e40ea4fdf32dfea97c49f
Erik <ewpeters@asu.edu>
2025-10-13 21:53:58 -0700

    add missing components to client

--- app/javascript/packs/mydrive-bundle.js
+++ app/javascript/packs/mydrive-bundle.js
@@ -4,31 +4,38 @@ import Album from '../bundles/MyDrive/components/album/Album';
 import AlbumsList from '../bundles/MyDrive/components/album/AlbumsList';
 import EditAlbum from '../bundles/MyDrive/components/album/EditAlbum';
 import NewAlbum from '../bundles/MyDrive/components/album/NewAlbum';
+
 import Photo from '../bundles/MyDrive/components/photo/Photo';
+import PhotosList from '../bundles/MyDrive/components/photo/PhotosList';
+import NewShare from '../bundles/MyDrive/components/photo/NewShare';
 import Comment from '../bundles/MyDrive/components/photo/Comment';
+import CommentsList from '../bundles/MyDrive/components/photo/CommentsList';
+
 import Login from '../bundles/MyDrive/components/auth/Login';
 import Signup from '../bundles/MyDrive/components/auth/Signup';
+import AllUsers from '../bundles/MyDrive/components/auth/AllUsers';
+
 import LandingPage from '../bundles/MyDrive/components/LandingPage';
 import RouterApp from '../bundles/MyDrive/components/RouterApp';
 import Layout from '../bundles/MyDrive/components/Layout';
-import AllUsers from '../bundles/MyDrive/components/auth/AllUsers';
-import CommentsList from '../bundles/MyDrive/components/photo/CommentsList';
 
 import './bootstrap.bundle.min.js';
 import './bootstrap.min.css';
 
 ReactOnRails.register({
   Album,
-  Photo,
-  Comment,
-  Login,
-  Signup,
-  LandingPage,
-  RouterApp,
-  Layout,
   AlbumsList,
   EditAlbum,
   NewAlbum,
+  Photo,
+  PhotosList,
+  NewShare,
+  Comment,
+  CommentsList,
+  Login,
+  Signup,
   AllUsers,
-  CommentsList
+  LandingPage,
+  RouterApp,
+  Layout
 });
